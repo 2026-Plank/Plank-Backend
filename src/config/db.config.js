c2c1af20994ae8ba9804bd22b18aebfa9415fb76
ygunk <ygunkun1015@gmail.com>
2026-06-18 01:43:57 +0900

    feat: db.config.js WALLET_DATA 추가

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -11,26 +11,24 @@ const connectDB = async () => {
   if (pool) return pool;
 
   try {
-    console.log('데이터베이스 연결 시도 중... (Easy Connect Mode)');
+    console.log('데이터베이스 연결 시도 중... (Wallet Buffer Mode)');
     
-    pool = await oracledb.createPool({
+    const poolOptions = {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       connectString: process.env.DB_CONNECT_STRING,
-      walletPassword: process.env.WALLET_PASSWORD,
-      
       poolMin: 2,
       poolMax: 10,
-      poolIncrement: 2,
-      queueTimeout: 10000, // 10초 대기
-      poolTimeout: 60,
-      poolPingInterval: 30,
+      queueTimeout: 10000,
+    };
 
-      // Render 서버와 Oracle 클라우드 간의 보안 접속(TLS) 병목을 해결하는 핵심 옵션
-      expireTime: 1,
-      sendTimeout: 10000,
-      recvTimeout: 10000
-    });
+    if (process.env.WALLET_DATA) {
+      console.log('[INFO] Render 환경 변수에서 WALLET_DATA 감지. mTLS 통신을 활성화합니다.');
+      poolOptions.walletPassword = process.env.WALLET_PASSWORD;
+      poolOptions.walletBuffer = Buffer.from(process.env.WALLET_DATA, 'base64');
+    }
+
+    pool = await oracledb.createPool(poolOptions);
 
     console.log('=== Oracle DB Pool Created Successfully! ===');
     return pool;
